afbc6324aec2f6a8b60f6e9a1a0b69d5a06fec16
ACHMADA FIQRI A. RASYAD <38800157+fiqriachmada@users.noreply.github.com>
2019-09-23 10:44:35 +0700

    Update SoalPendahuluan1_3.java

--- SoalPendahuluan1_3.java
+++ SoalPendahuluan1_3.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 public class NPM07066soalPendahuluan1_3 {
     public static void main (String[]args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Masukan nilai diamater =");
-        double diameter=input.nextInt();
-        System.out.println("masukan nilai tinggi = ");
-        double tinggi=input.nextInt();
+        System.out.print("Masukkan nilai diamater =");
+        double diameter=input.nextDouble();
+        System.out.print("Masukkan nilai tinggi = ");
+        double tinggi=input.nextDouble();
         double pi=3.14;
-        double volume= (diameter/2*pi*pi)*tinggi;
-        System.out.println("volume tabung = "+volume);
+        double vol= (diameter/2*pi*pi)*tinggi;
+        System.out.println("volume tabung = "+vol);
     }
 }
