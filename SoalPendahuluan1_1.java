1e8639fbda3819cbfabcea14a8b7177d49181729
ACHMADA FIQRI A. RASYAD <38800157+fiqriachmada@users.noreply.github.com>
2019-09-23 16:00:54 +0700

    Create SoalPendahuluan1_1.java

---
+++ SoalPendahuluan1_1.java
@@ -0,0 +1,8 @@
+public class NPM07066soalPendahuluan1_2 {
+    public static void main(String[] rgs){
+        int diameter=10;
+        double pi=3.14;
+        double luas=diameter/2*pi*pi;
+        System.out.print("Luas Lingkaran ="+luas);
+    }
+}
