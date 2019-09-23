e52f8d5b43bd2ab294bd842aa317572acf27f3b2
ACHMADA FIQRI A. RASYAD <38800157+fiqriachmada@users.noreply.github.com>
2019-09-23 15:24:36 +0700

    Create SoalPendahuluan1_5a.java

---
+++ SoalPendahuluan1_5a.java
@@ -0,0 +1,10 @@
+public class NPM07066tugasPendahuluan1_5a {
+    public static void main (String[]args){
+        for(int a=10;a>0;a--){
+            int d=a%2;
+            if(d==0){
+                System.out.println(a);
+            }
+        }
+    }
+}
